163c4fcf2111939bab1c3c772b7fe4099bdfdf18
Ayastian <se.ayalas@duocuc.cl>
2025-05-20 01:15:25 -0400

    Refactors Django settings for improved security and flexibility
    Updates the Django settings to enhance security by loading sensitive information from environment variables.
    
    Changes the secret key and debug settings to use environment variables, allowing for safer configurations in different environments.
    
    Also modifies allowed hosts to accept all, and sets a static root directory for collected static files.

--- admin-web/config/settings.py
+++ admin-web/config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%+qjjpaxgxx7y6=&an*=%_)jr*o7f2fj1pg5w5#-l0mf)+g8#i'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'insecure-in-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -80,7 +81,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-import os
 from dotenv import load_dotenv
 load_dotenv()
 DATABASES = {
@@ -130,9 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
